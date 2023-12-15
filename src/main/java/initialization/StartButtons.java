cc0d8fb0d290d13e4fd9265fcacb7761a5f371e1
Vadim Batyashov <changcheng39@gmail.com>
2023-12-15 18:30:20 +0200

    Add image, fix text, add button, fix buttons text

--- src/main/java/initialization/StartButtons.java
+++ src/main/java/initialization/StartButtons.java
@@ -1,28 +1,37 @@
 package initialization;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
-import java.util.Collections;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class StartButtons {
     public static InlineKeyboardMarkup setButtons() {
 
         InlineKeyboardButton buttonStart = InlineKeyboardButton
                 .builder()
-                .text("НЕОБМІННО ХОЧУ!")
+                .text("Перейти до курсу валют \uD83D\uDCB8")
                 .callbackData("1")
+                .build();
+
+        InlineKeyboardButton buttonUAH = InlineKeyboardButton
+                .builder()
+                .text("Про гривню \uD83C\uDDFA\uD83C\uDDE6")
+                .callbackData("2")
                 .build();
 
         InlineKeyboardButton buttonHelp = InlineKeyboardButton
                 .builder()
-                .text("ДІЗНАТИСЬ БІЛЬШЕ")
-                .callbackData("2")
+                .text("Що ще може бот-знайка \uD83E\uDD13")
+                .callbackData("3")
                 .build();
 
         InlineKeyboardMarkup keyboard = InlineKeyboardMarkup
                 .builder()
                 .keyboard(Collections.singletonList(Collections.singletonList(buttonStart)))
+                .keyboard(Collections.singletonList(Collections.singletonList(buttonUAH)))
                 .keyboard(Collections.singletonList(Collections.singletonList(buttonHelp)))
                 .build();
         return keyboard;
--- src/main/java/initialization/StartCommand.java
+++ src/main/java/initialization/StartCommand.java
@@ -2,10 +2,14 @@ package initialization;
 
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.File;
 
 public class StartCommand extends BotCommand {
 
@@ -15,12 +19,20 @@ public class StartCommand extends BotCommand {
 
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
-        String text = "Вітаю тебе на ринку валют! Чи хочеш дізнатись скільки доларів чи євро дають за рідну гривню?";
+        String text =
+                "Привіт! Тебе вітає ботик-знайка курсу валют! Я знаю все про поточний реальний курс головних валют до рідної гривні у світі!";
         SendMessage message = new SendMessage();
         message.setText(text);
         message.setChatId(chat.getId());
         message.setReplyMarkup(StartButtons.setButtons());
+        SendPhoto photo = new SendPhoto();
+        InputFile inputFile = new InputFile();
+        inputFile.setMedia(new File("src/main/java/initialization/image/photo.png"));
+        photo.setPhoto(inputFile);
+        photo.setChatId(chat.getId());
+
         try {
+            absSender.execute(photo);
             absSender.execute(message);
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
